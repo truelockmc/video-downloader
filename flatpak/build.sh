341fe13404f3dddda09efd573356e791e3c03b57
true_lock <168660625+truelockmc@users.noreply.github.com>
2026-02-12 21:56:35 +0100

    updated flatpak stuff

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-flatpak-builder --install-deps-from=flathub build-dir io.github.truelockmc.video-downloader.yaml --force-clean
+flatpak-builder --install-deps-from=flathub build-dir io.github.truelockmc.video-downloader.yml --force-clean
 
-flatpak-builder --user --install build-dir io.github.truelockmc.video-downloader.yaml --force-clean
-
-
+flatpak-builder --user --install build-dir io.github.truelockmc.video-downloader.yml --force-clean
--- flatpak/video-downloader.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/app/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/app/lib/python3.11/site-packages:$PYTHONPATH
-exec python3 /app/bin/main.py "$@"
